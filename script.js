03774cc9520b1f13044f781980069b8a9b53043f
Arvid Lindström <arvid-lindstrom@live.se>
2024-01-16 14:46:03 +0100

    formatting

--- script.js
+++ script.js
@@ -5,44 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
   var clickableCards = $(".flip");
 
   //flip card triggered when clicked
-  function checkCard(){
+  function checkCard() {
     var numFlipped = $(".flipIt").length; //check how many cards are flipped
-    if (numFlipped < 2){//if they are less than 2
+    if (numFlipped < 2) {//if they are less than 2
       this.classList.add("flipIt"); //flip the clicked card
       numFlipped = $(".flipIt").length; //check how many cards are flipped
-      }
-      if (numFlipped == 2){//when 2 or more are flipped:
-        $(".card").removeClass("flip");//make them not flippable
-        var flippedCards = $(".flipIt");//get the flipped cards
-        var allFlipped = $(".flipIt img");//get the imgs on the flipped cards
-        var arrImgs = Array.from(allFlipped);//make it an array
-        var src1 = arrImgs[0].attributes.src.value;//get src value
-        var src2 = arrImgs[1].attributes.src.value;//get src value
-        clickableCards = $(".flip");
+    }
+    if (numFlipped == 2) {//when 2 or more are flipped:
+      $(".card").removeClass("flip");//make them not flippable
+      var flippedCards = $(".flipIt");//get the flipped cards
+      var allFlipped = $(".flipIt img");//get the imgs on the flipped cards
+      var arrImgs = Array.from(allFlipped);//make it an array
+      var src1 = arrImgs[0].attributes.src.value;//get src value
+      var src2 = arrImgs[1].attributes.src.value;//get src value
+      clickableCards = $(".flip");
 
-        if (src1 == src2){//do the src match on flipped cards?
-          //if so then add .success and remove flipIt class on flipped cards
-          $("#msg").text("Success!");
-          flippedCards.addClass("success");
-          flippedCards.removeClass("flip");
-          $("#nR").attr("disabled", false);
-          $("#nR").focus();
-          }
-        else {//otherwise womp womp!
-          //print msg and activate next round button
-          $("#msg").text("Try again!");
-          $("#nR").prop("disabled", false);
-          $("#nR").focus();
-          }
-        }
-      else {
-        //if only one card is flipped, do nothing, wait for next card to be flipped
+      if (src1 == src2) {//do the src match on flipped cards?
+        //if so then add .success and remove flipIt class on flipped cards
+        $("#msg").text("Success!");
+        flippedCards.addClass("success");
+        flippedCards.removeClass("flip");
+        $("#nR").attr("disabled", false);
+        $("#nR").focus();
+      }
+      else {//otherwise womp womp!
+        //print msg and activate next round button
+        $("#msg").text("Try again!");
+        $("#nR").prop("disabled", false);
+        $("#nR").focus();
+      }
+    }
+    else {
+      //if only one card is flipped, do nothing, wait for next card to be flipped
     }
   };
-  
+
 
   //to reset the game and assign new imgs
-  function newGame(){
+  function newGame() {
     //ADD AN ALERT IF SOME CARDS ARE FLIPPED OR SUCCESS IS  MORE THAN 0 AND LESS THAN 16
     //IF NO DON'T
     //IF "YES" DO
@@ -54,62 +54,62 @@ document.addEventListener('DOMContentLoaded', () => {
     //$(".card").addClass("flip");
     //Array holding all image urls twice
     var myPix = ["assets/1.jpg",
-    "assets/2.jpg",
-    "assets/3.jpg",
-    "assets/4.jpg",
-    "assets/5.jpg",
-    "assets/6.jpg",
-    "assets/7.jpg",
-    "assets/8.jpg",
-    "assets/1.jpg",
-    "assets/2.jpg",
-    "assets/3.jpg",
-    "assets/4.jpg",
-    "assets/5.jpg",
-    "assets/6.jpg",
-    "assets/7.jpg",
-    "assets/8.jpg"];
+      "assets/2.jpg",
+      "assets/3.jpg",
+      "assets/4.jpg",
+      "assets/5.jpg",
+      "assets/6.jpg",
+      "assets/7.jpg",
+      "assets/8.jpg",
+      "assets/1.jpg",
+      "assets/2.jpg",
+      "assets/3.jpg",
+      "assets/4.jpg",
+      "assets/5.jpg",
+      "assets/6.jpg",
+      "assets/7.jpg",
+      "assets/8.jpg"];
     var imgSources = myPix;
     //to ensure each picture only gets chosen twice
-    function getImage(){
+    function getImage() {
       var rand = Math.floor(Math.random() * imgSources.length);
       var str = imgSources[rand];
-      imgSources.splice(rand,1);
+      imgSources.splice(rand, 1);
       return str;
-      }
+    }
     //run through all imgs and assign a src with getImage function
-    function assignImgs(){
-      for(i = 0; i < 16; i++){
-      var currentID = "#" + i;
-      var currentImg = "img" + currentID;
-      var img = $(currentImg);
-      img.attr('src', getImage());
+    function assignImgs() {
+      for (i = 0; i < 16; i++) {
+        var currentID = "#" + i;
+        var currentImg = "img" + currentID;
+        var img = $(currentImg);
+        img.attr('src', getImage());
       }
     }
     assignImgs();
     clickableCards.click(checkCard); //bind checkCard to clicking a card
   }
   //to initialize next round
-  function nextRound(){
+  function nextRound() {
     $(".flipIt").removeClass("flipIt");//flip back the cards
     $(".success").removeClass("flip");
     $("#msg").text(" ");//remove message
     $("#nR").prop("disabled", true);//disable the button
     roundsCnt++;//iterate round count
-    $("#rndCnt").text( "Number of rounds: " + roundsCnt);//print number of round
+    $("#rndCnt").text("Number of rounds: " + roundsCnt);//print number of round
     var numberOfWin = $(".success").length;
-      if (numberOfWin == 16){
-        $(".card").removeClass("flip");
-        $("#nR").prop("disabled", true);
-        $("#msg").text("START NEW GAME?");
-        }
-      else{
-        clickableCards = $(".flip");
-        clickableCards.click(checkCard); //bind checkCard to clicking a card
-      }
+    if (numberOfWin == 16) {
+      $(".card").removeClass("flip");
+      $("#nR").prop("disabled", true);
+      $("#msg").text("START NEW GAME?");
     }
+    else {
+      clickableCards = $(".flip");
+      clickableCards.click(checkCard); //bind checkCard to clicking a card
+    }
+  }
 
-    
-    $("#nG").click(newGame); //bind newGame to button
-    $("#nR").click(nextRound); //bind nextRound to button
+
+  $("#nG").click(newGame); //bind newGame to button
+  $("#nR").click(nextRound); //bind nextRound to button
 });
