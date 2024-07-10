1fd16d85f6329777116e88a5bda3208ed27aceb2
Arvid Lindström <arvid-lindstrom@live.se>
2024-07-10 20:34:27 +0200

    start work on initBoard

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  'use strict'
+  'use strict';
   const ui = {
     gameGrid: document.getElementById('game-grid'),
     newGame: document.getElementById('nG'),
@@ -14,17 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const numberOfCards = 16;
 
   const roundCounter = {
-    numberOfRounds: 0, 
-    increment: function(){
+    numberOfRounds: 0,
+    increment: function () {
       this.numberOfRounds += 1;
     },
-    reset: function(){
+    reset: function () {
       this.numberOfRounds = 0;
-    }
-   };
+    },
+  };
 
   function flipBackCards() {
-    ui.flippedCards().forEach((card) => card.classList.remove('flipIt'))
+    ui.flippedCards().forEach((card) => card.classList.remove('flipIt'));
   }
 
   function updateMessage(message) {
@@ -40,49 +40,50 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function checkCard(e) {
-    if (e.target.parentElement.classList.contains('flip')){
+    if (e.target.parentElement.classList.contains('flip')) {
       flipCard(e);
 
       if (numFlipped() == 2) {
-        
         let allFlipped = document.querySelectorAll('.flipIt i');
         let arrIs = Array.from(allFlipped);
-        
+
         let icon1 = arrIs[0].className;
         let icon2 = arrIs[1].className;
         if (icon1 === icon2) {
-          updateMessage("Success!");
+          updateMessage('Success!');
           ui.flippedCards().forEach((card) => card.classList.add('success'));
           ui.flippedCards().forEach((card) => card.classList.remove('flip'));
 
           if (ui.successCards().length == numberOfCards) {
             setTimeout(nextRound, 2000);
           }
+        } else {
+          updateMessage('Try again!');
         }
-        else {
-          updateMessage("Try again!");
-        }
-      }
-      else {
+      } else {
         nextRound();
         flipCard(e);
       }
     }
-  };
+  }
 
+  function initBoard(numberOfCards) {
+    // numberOfCards is selected 16, 25, 36 ?
+    // 
+  }
 
   function newGame() {
     //ADD AN ALERT IF SOME CARDS ARE FLIPPED OR SUCCESS IS MORE THAN 0 AND LESS THAN 16
     //IF NO DON'T
     //IF "YES" DO
-    updateMessage("Click the cards!");
+    updateMessage('Click the cards!');
     roundCounter.reset();
 
-    ui.roundCount.textContent = "Number of rounds: ";
-    ui.cards.forEach((card) => card.className = 'flip card');
+    ui.roundCount.textContent = 'Number of rounds: ';
+    ui.cards.forEach((card) => (card.className = 'flip card'));
 
     const icons = [
-      'fa-solid fa-brain', 
+      'fa-solid fa-brain',
       'fa-solid fa-marker',
       'fa-solid fa-walkie-talkie',
       'fa-solid fa-square-xmark',
@@ -123,26 +124,24 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.gameGrid.addEventListener('click', checkCard);
     ui.gameGrid.classList.remove('empty');
     ui.gameGrid.classList.add('active');
-
   }
 
-  //to initialize next round
   function nextRound() {
     flipBackCards();
 
     for (let card in ui.successCards().values) {
       card.classlist.remove('flip');
     }
-    
-    updateMessage(" ");
+
+    updateMessage(' ');
     roundCounter.increment();
     ui.roundCount.textContent = `Number of rounds: ${roundCounter.numberOfRounds}`;
     if (ui.successCards().length == 16) {
       ui.cards.forEach((card) => card.classList.remove('flip'));
-      updateMessage("START NEW GAME?");
+      updateMessage('START NEW GAME?');
     }
   }
 
   ui.gameGrid.addEventListener('click', checkCard);
-  ui.newGame.addEventListener('click', newGame); 
+  ui.newGame.addEventListener('click', newGame);
 });
